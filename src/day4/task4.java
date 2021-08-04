469f12c0f38735b2349071457d4c5a5a17d7d42a
Lenar Garipov <lenargaripoff@yandex.ru>
2021-08-04 21:46:51 +0300

    day4

--- src/day4/task4.java
+++ src/day4/task4.java
@@ -1,5 +1,10 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Random;
+
+import static java.util.Arrays.*;
+
 /**Создать новый массив размера 100 и заполнить его случайными числами из
  диапазона от 0 до 10000.
  Найти максимум среди сумм трех соседних элементов. Для найденной тройки с
@@ -17,6 +22,24 @@ package day4;
 
 public class task4 {
     public static void main(String[] args) {
+        int[] array = new int[100];
+        Random random = new Random();
 
+        for (int i = 0; i < array.length; i++)
+            array[i] = random.nextInt(10000);
+
+        int maxSum = 0;
+        int maxSumIndex = 0;
+        for (int i = 0; i < array.length - 2; i++) {
+            int sum = 0;
+            sum = array[i] + array[i + 1] + array[i + 2];
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxSumIndex = i;
+            }
+            System.out.println(maxSum);
+            System.out.println();
+            System.out.println(maxSumIndex);
+        }
     }
 }
